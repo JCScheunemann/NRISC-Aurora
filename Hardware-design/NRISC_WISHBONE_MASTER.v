76b43242821852eec4605ab30056cf004edd9538
Unknown <eveiga@msn.com>
2018-01-22 17:23:09 -0200

    definição do modulo caixa preta

--- Hardware-design/NRISC_WISHBONE_MASTER.v
+++ Hardware-design/NRISC_WISHBONE_MASTER.v
@@ -13,9 +13,11 @@ module NRISC_Wishbone_master (
 			WSHMSTR_ACK,		// assert: ok, else err+rty
 			WSHMSTR_ERR,		// terminação anormal de ciclo
 			WSHMSTR_RTY,		// interrompe pra retransmissão
-			WSHMSTR_TGDIN,		// tags associadas ao vetor DATAOUT
+			WSHMSTR_TGDIN,		// tags associadas ao vetor DATAIN
+			WSHMSTR_TGDOUT,		// tag associada ao vetor DATAOUT
 			WSHMSTR_RSTIN,		// rst input
 			WSHMSTR_CLKIN,		// clk input
+			WSHMSTR_CLKOUT,		// clk output
 			WSHMSTR_DATAIN,		// autoexplicativo
 			WSHMSTR_DATAOUT,	// idem
 			WSHMSTR_RSTOUT, 	// dispara um comando de reset
@@ -25,9 +27,40 @@ module NRISC_Wishbone_master (
 			WSHMSTR_LOCK,		// impede a interrupção do ciclo de transmissão
 			WSHMSTR_SEL,		// vetor indica onde dados validos são experados no sinal datain durante ciclos read
 			WSHMSTR_TGAOUT,		// tag com linha de endereço, validação pelo strobe
-			WSHMSTR_TGCIN,		// tags de descrição de ciclo, qualificada pelo cycout
+			WSHMSTR_TGCOUT,		// tags de descrição de ciclo, qualificada pelo cycout
 			WSHMSTR_WREN		// write enable. 0 write 1 read
 		);
 
 	parameter N_DData = `N_DData; //2^N posicoes
 	parameter TAM=`TAM;
+
+	// ======= INPUTS ==========
+	
+//	 i/o 	w/r 	width		name
+
+	input 	wire 				WSHMSTR_CLKIN;
+	input 	wire 				WSHMSTR_RSTIN;
+	input 	reg 	[TAM-1:0]	WSHMSTR_DATAIN;
+	input 	reg		[7:0]		WSHMSTR_TGDIN;
+	input 	wire 				WSHMSTR_ACK;
+	input 	wire 				WSHMSTR_ERR;
+	input 	wire 				WSHMSTR_RTY;
+	
+	// ======= OUTPUTS ==========
+	
+//	 i/o 	w/r 	width		name
+
+	output 	reg					WSHMSTR_WREN;
+	output 	reg		[TAM-1:0]	WSHMSTR_ADDR_OUT;
+	output 	reg					WSHMSTR_CYC_OUT;
+	output 	reg					WSHMSTR_LOCK;
+	output 	reg		[TAM-1:0]	WSHMSTR_SEL;
+	output 	reg		[7:0]		WSHMSTR_TGAOUT;
+	output 	reg		[7:0]		WSHMSTR_TGCOUT;
+	output 	reg		[7:0]		WSHMSTR_TGDOUT;
+	output 	wire 				WSHMSTR_STRBOUT;
+	output 	reg					WSHMSTR_CLKOUT;
+	output 	reg 				WSHMSTR_RSTOUT;
+	output 	reg		[TAM-1:0]	WSHMSTR_DATAOUT;
+
+// ======= oh god what now where do i start =====
--- Hardware-design/NRISC_WISHBONE_SLAVE.v
+++ Hardware-design/NRISC_WISHBONE_SLAVE.v
@@ -10,24 +10,53 @@
 `timescale 1ns/1ns
 
 module NRISC_Wishbone_slave (
-			WSHSLAV_ACK,		// assert: ok, else err+rty
-			WSHSLAV_ERR,		// terminação anormal de ciclo
-			WSHSLAV_RTY,		// interrompe pra retransmissão
-			WSHSLAV_TGDIN,		// tags associadas ao vetor DATAOUT
-			WSHSLAV_RSTIN,		// rst input
-			WSHSLAV_CLKIN,		// clk input
-			WSHSLAV_DATAIN,		// autoexplicativo
-			WSHSLAV_DATAOUT,	// idem
-			WSHSLAV_RSTOUT, 	// dispara um comando de reset
-			WSHSLAV_ADDR_OUT,	// despacha endereço
-			WSHSLAV_CYC_OUT,	// transmissão válida
-			WSHSLAV_STRBOUT,	// sinal de validação
-			WSHSLAV_LOCK,		// impede a interrupção do ciclo de transmissão
-			WSHSLAV_SEL,		// vetor indica onde dados validos são experados no sinal datain durante ciclos read
-			WSHSLAV_TGAOUT,		// tag com linha de endereço, validação pelo strobe
-			WSHSLAV_TGCIN,		// tags de descrição de ciclo, qualificada pelo cycout
-			WSHSLAV_WREN		// write enable. 0 write 1 read
+			WSHSLAV_CLKIN,
+			WSHSLAV_RSTIN,
+			WSHSLAV_TGDIN,
+			WSHSLAV_DATAIN,
+			WSHSLAV_CLKOUT,
+			WSHSLAV_RSTOUT,
+			WSHSLAV_DATAOUT,
+			WSHSLAV_TGDOUT,
+			WSHSLAV_ADDRIN,
+			WSHSLAV_LOCKIN,
+			WSHSLAV_SEL,
+			WSHSLAV_STROBE,
+			WSHSLAV_TGA,
+			WSHSLAV_TGC,
+			WSHSLAV_WREN,
+			WSHSLAV_RETRY,
+			WSHSLAV_ERROUT,
+			WSHSLAV_ACK
 		);
 
 	parameter N_DData = `N_DData; //2^N posicoes
 	parameter TAM=`TAM;
+
+		// ======= INPUTS ==========
+	
+//	 i/o 	w/r 	width		name
+
+	input 	wire 				WSHSLAV_LOCKIN;
+	input 	wire 				WSHSLAV_STROBE;
+	input 	reg 	[7:0]		WSHSLAV_TGA;
+	input 	reg		[7:0]		WSHSLAV_TGC;	
+	input 	reg		[7:0]		WSHSLAV_TGDIN;	
+	input 	wire 				WSHSLAV_WREN;
+	input 	reg		[TAM-1:0]	WSHSLAV_ADDRIN; 				
+	input 	reg		[TAM-1:0] 	WSHSLAV_SEL;
+	input 	reg		[TAM-1:0]	WSHSLAV_DATAIN;
+	input 	reg					WSHSLAV_CLKIN;
+	input 	reg					WSHSLAV_RSTIN;
+
+			// ======= OUTPUT ==========
+	
+//	 i/o 	w/r 	width		name
+
+	output 	wire 				WSHSLAV_RETRY;
+	output 	wire 				WSHSLAV_ERROUT;
+	output 	wire 				WSHSLAV_ACK;
+	output 	reg		[7:0]		WSHMSTR_TGDOUT;
+	output 	reg					WSHMSTR_CLKOUT;
+	output 	reg 				WSHMSTR_RSTOUT;
+	output 	reg		[TAM-1:0]	WSHMSTR_DATAOUT;
